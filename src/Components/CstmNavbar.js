8edd3667182c0157f4d119e2109b3fbf1142b27b
Ervan Andi <ervan.andi45@gmail.com>
2021-09-24 19:10:10 +0700

    more customization

--- src/Components/CstmNavbar.js
+++ src/Components/CstmNavbar.js
@@ -22,39 +22,22 @@ export default function CstmNavbar() {
     return (
 
         <Navbar className="row" light expand="md">
-          <NavbarBrand className="col">
+          <NavbarBrand className="col-6">
             <Link style={{textDecoration:"none", color:"white"}} to="/">
               <h1><b>MY MOVIE LIST</b></h1>
             </Link>
             </NavbarBrand>
           <NavbarToggler onClick={toggle} />
-          <Collapse className="col bg-white rounded" isOpen={isOpen} navbar>
+          <Collapse className="col-1" isOpen={isOpen} navbar>
             <Nav className="mr-auto" navbar>
               <NavItem>
                 <Link to="/upcoming">
-                  <NavLink>Upcoming Movie</NavLink>
+                  <NavLink className="text-white">Upcoming Movie</NavLink>
                 </Link>
               </NavItem>
               <NavItem>
-                <NavLink>GitHub</NavLink>
+                <NavLink className="text-white">About Me</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
